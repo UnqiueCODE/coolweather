e8aa21ffff829cfd13febba7f3210696dd5ad8c1
steven <384268596@qq.com>
2017-03-06 19:29:38 +0800

    加入创建库和表的各项配置

--- app/src/main/java/com/coolweather/android/db/City.java
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -3,37 +3,44 @@ package com.coolweather.android.db;
 import org.litepal.crud.DataSupport;
 
 /**
- * Created by Steven on 2017/3/5.
+ * Created by Steven on 2017/3/6.
  */
 
 public class City extends DataSupport {
     private int id;
     private String cityName;
     private int cityCode;
-    private int provinceCode;
+    private int provinceId;
 
-    public int getId(){
+    public int getId() {
         return id;
     }
-    public void setId(int id){
+
+    public void setId(int id) {
         this.id = id;
     }
-    public String getCityName(){
+
+    public String getCityName() {
         return cityName;
     }
-    public void setCityName(String cityName){
+
+    public void setCityName(String cityName) {
         this.cityName = cityName;
     }
-    public int getCityCode(){
+
+    public int getCityCode() {
         return cityCode;
     }
-    public void setCityCode(int cityCode){
+
+    public void setCityCode(int cityCode) {
         this.cityCode = cityCode;
     }
-    public int getProvinceCode(){
-        return provinceCode;
+
+    public int getProvinceId() {
+        return provinceId;
     }
-    public void setProvinceCode(int provinceCode){
-        this.provinceCode = provinceCode;
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
     }
 }
